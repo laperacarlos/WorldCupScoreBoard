2ddbccbebb42b89e53f76daa91c8725461980e8e
kgruszczynski <kgruszczynski@bravurasolutions.com>
2024-10-01 18:53:09 +0200

    hello world cup!

---
+++ src/main/java/lapera/world/cup/WorldCupScoreBoardMain.java
@@ -0,0 +1,7 @@
+package lapera.world.cup;
+
+public class WorldCupScoreBoardMain {
+    public static void main(String[] args) {
+        System.out.println("Hello World Cup Score Board!");
+    }
+}
